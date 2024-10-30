393721ef160bf50efca7b1b837c35239a9c1a4fa
LE2D22yanoura <k023g0120@g.neec.ac.jp>
2024-10-30 10:49:41 +0900

    プロジェクト ファイルを追加します。

---
+++ GameScene.cpp
@@ -0,0 +1,33 @@
+#include "GameScene.h"
+
+class GameScene {
+public:
+	/// <summary>
+	/// コンストクラタ
+	/// </summary>
+	GameScene();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~GameScene();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+private:
+	DirectXCommon* dxCommon_ = nullptr;
+
+};
