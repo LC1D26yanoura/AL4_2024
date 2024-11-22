c8ce0b9a8cdd8ea1bc36ae390e1e3c89a830ae2c
LE2D22yanoura <k023g0120@g.neec.ac.jp>
2024-11-22 12:32:35 +0900

    06ブランチ

---
+++ Enemy.cpp
@@ -0,0 +1,7 @@
+#include "Enemy.h"
+
+void Enemy::Initialize() {}
+
+void Enemy::Update() {}
+
+void Enemy::Draw() {}
---
+++ Enemy.h
@@ -0,0 +1,18 @@
+#pragma once
+class Enemy {
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+};
